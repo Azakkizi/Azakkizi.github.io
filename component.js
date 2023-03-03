1fbedaf55a9046d2eca7b6400b9243eb66df6cf3
Betül Azakoğlu <61990656+Azakkizi@users.noreply.github.com>
2023-03-04 01:27:35 +0300

    completely changed and renewed

---
+++ component.js
@@ -0,0 +1,30 @@
+class MyNavbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 
+        `
+        <div class="navbar">
+        <div class="logo">
+        <a>Azakkızı</a>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="resume.html">Resume</a></li>
+            <li><a href="portfolio.html">Portfolio</a></li>
+            <li><a href="connects.html">Connects</a></li>
+        </ul>
+    </div>
+    <div class="contact-info">
+       <ul>
+           <li><a class="phone-no">+90 (538) 240 74 00 </a></li>
+           <li><a class="sep"> / </a></li>
+           <li><a class="email">betus.azak.95@gmail.com </a></li>
+       </ul>
+       
+   </div>
+   </div>
+   `
+    }
+}
+customElements.define('my-navbar', MyNavbar)
